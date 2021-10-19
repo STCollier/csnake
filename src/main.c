9cb4d7fb896889f64d2c692a3d46414d080b91ca
Tony Mackay <tonymackay@pm.me>
2021-10-19 17:05:19 +0100

    fix: Prevent Snake eating itself from rapid input

--- src/main.c
+++ src/main.c
@@ -34,30 +34,36 @@ void draw_food(void);
 void play_again(void);
 
 typedef enum Game_State {
-    NOT_PLAYING = 0,
-    PLAYING = 1,
-    PAUSED = 2,
-    GAME_OVER = 3
+  NOT_PLAYING = 0,
+  PLAYING = 1,
+  PAUSED = 2,
+  GAME_OVER = 3
 } Game_State;
+
+typedef struct Snake {
+  SDL_FPoint head;
+  SDL_Rect body[CELL_COUNT];
+  SDL_KeyCode current_direction;
+  SDL_KeyCode skipped;
+  int direction_can_change;
+  float dx;
+  float dy;
+} Snake;
 
 typedef struct {
   SDL_Renderer *renderer;
 	SDL_Window *window;
-  SDL_FPoint head;
-  SDL_Rect snake[CELL_COUNT];
-  int running;
-  float dx;
-  float dy;
-  SDL_Rect food;
-  int score;
   Game_State state;
+  Snake snake;
+  SDL_Rect food;
+  int running;
+  int score;
 } Game;
 
 // initialize global structure to store game state
 // and SDL renderer for use in all functions
 Game game = {
   .running = 1,
-  .dx = SNAKE_SPEED,
   .food = {
     .w = CELL_WIDTH, .h = CELL_HEIGHT
   },
@@ -245,9 +251,9 @@ void draw_walls(void) {
 
 void draw_snake(void) {
   // draw snake body
-  for (int i = 1; i < sizeof(game.snake)/sizeof(game.snake[0]); i++) {
+  for (int i = 1; i < sizeof(game.snake.body)/sizeof(game.snake.body[0]); i++) {
     // only draw active elements (assigned a width)
-    if (game.snake[i].w == 0) {
+    if (game.snake.body[i].w == 0) {
       break;
     }
     // Make the snake red when it's dead
@@ -257,11 +263,11 @@ void draw_snake(void) {
       // draw green cell
       SDL_SetRenderDrawColor(game.renderer, 0, 128, 0, 255);
     }
-    SDL_RenderFillRect(game.renderer, &game.snake[i]);
+    SDL_RenderFillRect(game.renderer, &game.snake.body[i]);
 
     // create a black border around each snake body cell
     SDL_SetRenderDrawColor(game.renderer, 0, 0, 0, 255);
-    SDL_RenderDrawRect(game.renderer, &game.snake[i]);
+    SDL_RenderDrawRect(game.renderer, &game.snake.body[i]);
   }
   // draw the snake head
   if (game.state == GAME_OVER) {
@@ -270,30 +276,35 @@ void draw_snake(void) {
     // draw green cell
     SDL_SetRenderDrawColor(game.renderer, 0, 128, 0, 255);
   }
-  SDL_RenderFillRect(game.renderer, &game.snake[0]);
+  SDL_RenderFillRect(game.renderer, &game.snake.body[0]);
 }
 
 void spawn_snake(void) {
+  game.snake.current_direction = SDLK_RIGHT;
+  game.snake.direction_can_change = 1;
+  game.snake.dx = SNAKE_SPEED;
+  game.snake.dy = 0;
+
   // make all snake parts empty
-  for (int i = 0; i < sizeof(game.snake)/sizeof(game.snake[0]); i++) {
-    game.snake[i].x = 0;
-    game.snake[i].y = 0;
-    game.snake[i].w = 0;
-    game.snake[i].h = 0;
+  for (int i = 0; i < sizeof(game.snake.body)/sizeof(game.snake.body[0]); i++) {
+    game.snake.body[i].x = 0;
+    game.snake.body[i].y = 0;
+    game.snake.body[i].w = 0;
+    game.snake.body[i].h = 0;
   }
 
   // the first element in the snake array is the head
-  game.head.x = SNAKE_START_X;
-  game.head.y = SNAKE_START_Y;
-  game.snake[0].x = SNAKE_START_X;
-  game.snake[0].y = SNAKE_START_Y;
-  game.snake[0].w = CELL_WIDTH;
-  game.snake[0].h = CELL_HEIGHT;
+  game.snake.head.x = SNAKE_START_X;
+  game.snake.head.y = SNAKE_START_Y;
+  game.snake.body[0].x = SNAKE_START_X;
+  game.snake.body[0].y = SNAKE_START_Y;
+  game.snake.body[0].w = CELL_WIDTH;
+  game.snake.body[0].h = CELL_HEIGHT;
 
   // create 4 more snake elements for the body
   for (int i = 1; i < 5; i++) {
-    game.snake[i] = game.snake[0];
-    game.snake[i].x = game.snake[0].x - (CELL_WIDTH * i);
+    game.snake.body[i] = game.snake.body[0];
+    game.snake.body[i].x = game.snake.body[0].x - (CELL_WIDTH * i);
   }
 }
 
@@ -303,14 +314,14 @@ void move_snake(void) {
     return;
   }
 
-  game.head.x = game.head.x + game.dx;
-  game.head.y = game.head.y + game.dy;
+  game.snake.head.x += game.snake.dx;
+  game.snake.head.y += game.snake.dy;
 
   // work out if the new x and y has moved into a new grid cell
-  int old_x = (int)round(game.snake[0].x / CELL_WIDTH) * CELL_WIDTH;
-  int new_x = (int)round(game.head.x / CELL_WIDTH) * CELL_WIDTH;
-  int old_y = (int)round(game.snake[0].y / CELL_HEIGHT) * CELL_HEIGHT;
-  int new_y = (int)round(game.head.y / CELL_HEIGHT) * CELL_HEIGHT;
+  int old_x = (int)round(game.snake.body[0].x / CELL_WIDTH) * CELL_WIDTH;
+  int new_x = (int)round(game.snake.head.x / CELL_WIDTH) * CELL_WIDTH;
+  int old_y = (int)round(game.snake.body[0].y / CELL_HEIGHT) * CELL_HEIGHT;
+  int new_y = (int)round(game.snake.head.y / CELL_HEIGHT) * CELL_HEIGHT;
 
   if (old_x == new_x && old_y == new_y) {
     // Snake head hasn't moved into a new grid cell
@@ -318,95 +329,111 @@ void move_snake(void) {
   }
 
   // shift elements to right to make room for new head
-  for (int i = sizeof(game.snake)/sizeof(game.snake[0])-1; i >= 0; i--) {
-    game.snake[i] = game.snake[i-1];
+  for (int i = sizeof(game.snake.body)/sizeof(game.snake.body[0])-1; i >= 0; i--) {
+    game.snake.body[i] = game.snake.body[i-1];
   }
   // insert new head position at the begining
-  game.snake[0].x = new_x;
-  game.snake[0].y = new_y;
-  game.snake[0].w = CELL_WIDTH;
-  game.snake[0].h = CELL_HEIGHT;
+  game.snake.body[0].x = new_x;
+  game.snake.body[0].y = new_y;
+  game.snake.body[0].w = CELL_WIDTH;
+  game.snake.body[0].h = CELL_HEIGHT;
 
   // if the head touces the snake, make it grow by not removing the tail
   // and spawn the food in a new random location.
-  if (game.food.x == game.snake[0].x && game.food.y == game.snake[0].y) {
+  if (game.food.x == game.snake.body[0].x && game.food.y == game.snake.body[0].y) {
     spawn_food();
     game.score++;
   } else {
     //remove the tail by finding the last inactive element in the Snake array
     //then zeroing out the one before it.
-    for (int i = 5; i < sizeof(game.snake)/sizeof(game.snake[0]); i++) {
-      if (game.snake[i].w == 0) {
-        game.snake[i-1].x = 0;
-        game.snake[i-1].y = 0;
-        game.snake[i-1].w = 0;
-        game.snake[i-1].h = 0;
+    for (int i = 5; i < sizeof(game.snake.body)/sizeof(game.snake.body[0]); i++) {
+      if (game.snake.body[i].w == 0) {
+        game.snake.body[i-1].x = 0;
+        game.snake.body[i-1].y = 0;
+        game.snake.body[i-1].w = 0;
+        game.snake.body[i-1].h = 0;
         break;
       }
     }
   }
+
+  // allow the direction to change now that snake has moved 
+  game.snake.direction_can_change = 1;
+
+  // if player tried to change direction too quick we could have crashed back 
+  // into ourself, but now snake has moved change the direction with the
+  // queued input
+  if (game.snake.skipped != -1) {
+    change_direction(game.snake.skipped);
+    game.snake.skipped = -1;
+  }
+
   handle_collisions();
 }
 
 void change_direction(SDL_KeyCode new_direction) {
-  // work out what direction the snake is going
-  int going_up = game.dy == -CELL_HEIGHT;
-  int going_down = game.dy == CELL_HEIGHT;
-  int going_left = game.dx == -CELL_WIDTH;
-  int going_right = game.dx == CELL_WIDTH;
-
-  // change the direction to up when the snake is not going down
-  if (new_direction == SDLK_UP && !going_down) {
-      game.dx = 0;
-      game.dy = -SNAKE_SPEED;
+  if (!game.snake.direction_can_change) {
+    printf("queue direction change until snake has moved\n");
+    game.snake.skipped = new_direction;
+    return;
   }
-  // change the direction to down when the snake is not going up
-  if (new_direction == SDLK_DOWN && !going_up) {
-      game.dx = 0;
-      game.dy = SNAKE_SPEED;
-  }
-  // change the direction to left when the snake is not going right
-  if (new_direction == SDLK_LEFT && !going_right) {
-      game.dx = -SNAKE_SPEED;
-      game.dy = 0;
-  }
-  // change the direction to right when the snake is not going left
-  if (new_direction == SDLK_RIGHT && !going_left) {
-      game.dx = SNAKE_SPEED;
-      game.dy = 0;
+  if (new_direction == SDLK_UP && game.snake.current_direction != SDLK_DOWN) {
+    game.snake.dx = 0;
+    game.snake.dy = -SNAKE_SPEED;
+    game.snake.current_direction = SDLK_UP;
+    game.snake.direction_can_change = 0;
+    printf("UP\n");
+  } else if (new_direction == SDLK_DOWN && game.snake.current_direction != SDLK_UP) {
+    game.snake.dx = 0;
+    game.snake.dy = SNAKE_SPEED;
+    game.snake.current_direction = SDLK_DOWN;
+    game.snake.direction_can_change = 0;
+    printf("DOWN\n");
+  } else if (new_direction == SDLK_LEFT && game.snake.current_direction != SDLK_RIGHT) {
+    game.snake.dx = -SNAKE_SPEED;
+    game.snake.dy = 0;
+    game.snake.current_direction = SDLK_LEFT;
+    game.snake.direction_can_change = 0;
+    printf("LEFT\n");
+  } else if (new_direction == SDLK_RIGHT && game.snake.current_direction != SDLK_LEFT) {
+    game.snake.dx = SNAKE_SPEED;
+    game.snake.dy = 0;
+    game.snake.current_direction = SDLK_RIGHT;
+    game.snake.direction_can_change = 0;
+    printf("RIGHT\n");
   }
 }
 
 void handle_collisions(void) {
   // hit snake?
-  for (int i = 1; i < sizeof(game.snake)/sizeof(game.snake[0]); i++) {
+  for (int i = 1; i < sizeof(game.snake.body)/sizeof(game.snake.body[0]); i++) {
     // exit loop when at the end of the active elements of the snake body
-    if (game.snake[i].w == 0) {
+    if (game.snake.body[i].w == 0) {
       break;
     }
     // check the head has not run into active body elements
-    if (game.snake[0].x == game.snake[i].x && game.snake[0].y == game.snake[i].y) {
+    if (game.snake.body[0].x == game.snake.body[i].x && game.snake.body[0].y == game.snake.body[i].y) {
       game.state = GAME_OVER;
       return;
     }
   }
   // hit lift wall?
-  if (game.snake[0].x < WALL_THICKNESS) {
+  if (game.snake.body[0].x < WALL_THICKNESS) {
     game.state = GAME_OVER;
     return;
   }
   // hit right wall?
-  if (game.snake[0].x > SCREEN_WIDTH - WALL_THICKNESS - CELL_WIDTH) {
+  if (game.snake.body[0].x > SCREEN_WIDTH - WALL_THICKNESS - CELL_WIDTH) {
     game.state = GAME_OVER;
     return;
   }
   // hit top wall?
-  if (game.snake[0].y < WALL_THICKNESS) {
+  if (game.snake.body[0].y < WALL_THICKNESS) {
     game.state = GAME_OVER;
     return;
   }
   // hit bottoom wall?
-  if (game.snake[0].y > SCREEN_HEIGHT - WALL_THICKNESS - CELL_HEIGHT) {
+  if (game.snake.body[0].y > SCREEN_HEIGHT - WALL_THICKNESS - CELL_HEIGHT) {
     game.state = GAME_OVER;
     return;
   }
@@ -431,12 +458,12 @@ void spawn_food() {
   }
 
   // only spawn the food if it does not touch the snake
-  for (int i = 0; i < sizeof(game.snake)/sizeof(game.snake[0]); i++) {
+  for (int i = 0; i < sizeof(game.snake.body)/sizeof(game.snake.body[0]); i++) {
     // exit loop when at the end of the active elements of the snake body
-    if (game.snake[i].w == 0) {
+    if (game.snake.body[i].w == 0) {
       break;
     }
-    if (game.snake[i].x == game.food.x && game.snake[i].y == game.food.y) {
+    if (game.snake.body[i].x == game.food.x && game.snake.body[i].y == game.food.y) {
       spawn_food();
       break;
     }
@@ -457,8 +484,6 @@ void draw_food() {
 }
 
 void play_again(void) {
-  game.dx = SNAKE_SPEED;
-  game.dy = 0;
   game.score = 0;
   game.state = PLAYING;
   spawn_snake();
